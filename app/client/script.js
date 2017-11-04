7bb6098233dc8549283a3854087278a268818bca
Philip Scott <pscott@zeptohost.com>
2017-11-04 09:01:54 -0400

    Added Google map

--- app/client/script.js
+++ app/client/script.js
@@ -1,3 +1,6 @@
+var btn = $('#displayCoords')
+var lbl = $('#coordLbl')
+
 var x = $('#demo')
 var btn = $('#update')
 
@@ -5,40 +8,46 @@ btn.addEventListener('click', getLocation)
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(initMap)
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    lbl.innerHTML = "Geolocation is not supported by this browser."
   }
 }
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
+function displayLocation (position) {
+  updateLabel(position)
+  initMap(position)
+}
+function updateLabel (position) {
+  lbl.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
 }
 
 
-// testing live update
-function initMap () {}
-window.onload = function () {
-  initMap = function() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map( $('#display') , myOptions);
+function initMap (position) {
+  position = position || {
+    coords: {
+      latitude: -25.363,
+      longitude: 131.044
+    }
   }
+  var uluru = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  })
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  })
 }
 
 
 
 
-
-
-
-// library stuff
+// general library code
 
 function $ (s) {
   return document.querySelector(s)
